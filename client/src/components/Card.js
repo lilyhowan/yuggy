91569f6d5b6730f7938a749c713652994fe9c582
Lily Howan <lily.howan@gmail.com>
2022-01-30 16:20:38 +1300

    Add filter for archetype

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,77 +1,84 @@
 const Card = (props) => {
-    // effect monsters that are also another monster type (e.g. synchro/effect) use the colour of the other type
-    // pendulum monsters use a vertical gradient that fades from the other monster type's card color to spell type green
+  // effect monsters that are also another monster type (e.g. synchro/effect) use the colour of the other type
+  // pendulum monsters use a vertical gradient that fades from the other monster type's card color to spell type green
 
-    const TYPE_COLORS = {
-        "Effect Monster": "#FF8B53",
-        "Normal Monster" : "#FDE68A",
-        "Spell Card": "#1D9E74",
-        "Pendulum Effect Monster": "#1D9E74",
-        "Ritual Effect Monster": "#9DB5CC",
-        "Link Monster": "#00008B",
-        "Fusion Monster": "#A086B7",
-        "Trap Card": "#BC5A84",
-        "Synchro Monster": "#CCCCCC",
-        "Token Monster": "#C0C0C0",
-        "XYZ Monster": "#000000"
+  const getCardColor = (type) => {
+    if (type.includes("Pendulum") || type.includes("Spell")) {
+      return "green";
+    } else if (type.includes("Normal")) {
+      return "yellow";
+    } else if (type.includes("Trap")) {
+      return "purple";
+    } else if (type.includes("Fusion")) {
+      return "violet";
+    } else if (type.includes("Link")) {
+      return "darkBlue";
+    } else if (type.includes("Ritual")) {
+      return "lightBlue";
+    } else if (type.includes("Synchro")) {
+      return "white";
+    } else if (type.includes("XYZ")) {
+      return "black";
+    } else if (type.includes("Skill")) {
+        return "blue";
+    } else if (
+      type.includes("Effect") ||
+      type.includes("Gemini") ||
+      type.includes("Toon") ||
+      type.includes("Spirit") ||
+      type.includes("Tuner")
+    ) {
+      return "orange";
+    } else {
+      return "NOT_FOUND";
     }
+  };
 
-    const COLORS = {
-        "orange": "#FF8B53",
-        "yellow": "#FDE68A",
-        "green": "#1D9E74",
-        "lightBlue": "#9DB5CC",
-        "darkBlue": "#00008B",
-        "violet": "#A086B7",
-        "purple": "#BC5A84",
-        "white": "#CCCCCC",
-        "gray": "#C0C0C0",
-        "black": "#000000",
-    }
+  const TYPE_COLORS = {
+    NOT_FOUND: "#FFFFFF",
+    orange: "#FF8B53",
+    yellow: "#FDE68A",
+    green: "#1D9E74",
+    blue: "#01AAF5",
+    lightBlue: "#9DB5CC",
+    darkBlue: "#00008B",
+    violet: "#A086B7",
+    purple: "#BC5A84",
+    white: "#CCCCCC",
+    gray: "#C0C0C0",
+    black: "#000000"
+  };
 
-    const TYPES = {
-        "Effect Monster": "orange",
-        "Flip Effect Monster": "",
-        "Flip Tuner Effect Monster": "",
-        "Gemini Monster": "",
-        "Normal Monster": "yellow",
-        "Normal Tuner Monster": "",
-        "Pendulum Effect Monster": "",
-        "Pendulum Flip Effect Monster": "",
-        "Pendulum Normal Monster": "",
-        "Pendulum Tuner Effect Monster": "",
-        "Ritual Effect Monster": "",
-        "Ritual Monster": "",
-        "Skill Card": "",
-        "Spell Card": "green",
-        "Spirit Monster": "",
-        "Toon Monster": "orange",
-        "Trap Card": "purple",
-        "Tuner Monster": "",
-        "Union Effect Monster": "",
-        "Fusion Monster": "violet",
-        "Link Monster": "darkBlue",
-        "Pendulum Effect Fusion Monster": "",
-        "Synchro Monster": "white",
-        "Synchro Pendulum Effect Monster": "",
-        "Synchro Tuner Monster": "",
-        "XYZ Monster": "",
-        "XYZ Pendulum Effect Monster": "",
-    }
-
-    return (
-        <div className="Card flex flex-col items-start max-w-xs p-4 gap-4 rounded-md" style={{ background: `linear-gradient(180deg, ${TYPE_COLORS[props.type]} 45%, #2B292C 45%)` }}>
-            <img className="rounded container mx-auto" src={props.url} alt={props.name} />
-            <div className="card-info text-left">
-                <p className="font-bold">{props.name}</p>
-                <ul>
-                    <li>{props.type}</li>
-                    {props.atk && <li>ATK: {props.atk} / DEF: {props.def}</li>}
-                    <li>Owned: {props.owned} / Ran: {props.ran}</li>
-                </ul>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div
+      className="Card flex flex-col items-start max-w-xs p-4 gap-4 rounded-md"
+      style={{
+        background: `linear-gradient(180deg, ${
+          TYPE_COLORS[getCardColor(props.type)]
+        } 45%, #2B292C 45%)`
+      }}
+    >
+      <img
+        className="rounded container mx-auto"
+        src={props.url}
+        alt={props.name}
+      />
+      <div className="card-info text-left">
+        <p className="font-bold">{props.name}</p>
+        <ul>
+          <li>{props.type}</li>
+          {props.atk && (
+            <li>
+              ATK: {props.atk} / DEF: {props.def}
+            </li>
+          )}
+          <li>
+            Owned: {props.owned} / Ran: {props.ran}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,38 +1,117 @@
-import Card from './Card';
-import React, { useEffect, useState } from 'react';
+import Card from "./Card";
+import cardData from "../assets/cardData.json";
+import React, { useEffect, useState } from "react";
 
 function Search() {
   const [cards, setCards] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [typeQuery, setTypeQuery] = useState("");
+  const [archetypeQuery, setArchetypeQuery] = useState("");
+  const [archetypes, setArchetypes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://db.ygoprodeck.com/api/v7/archetypes.php")
+      .then((response) => response.json())
+      .then((data) => {
+        const archetypeArr = [];
+        data.map((value) => {
+          archetypeArr.push(value.archetype_name);
+        });
+        setArchetypes(archetypeArr);
+      });
+  }, []);
 
   useEffect(() => {
     const timeOut = setTimeout(() => {
-      console.log('Searching...')
-      fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=${searchQuery}&type=${typeQuery}`)
-      .then(response => response.json())
-      .then(({ data: cards }) => {
-        setCards(cards);
-      });
+      let queryParameters = [];
+
+      if (searchQuery !== "") {
+        queryParameters.push(`fname=${searchQuery}`);
+      }
+
+      if (typeQuery !== "") {
+        queryParameters.push(`type=${typeQuery}`);
+      }
+
+      if (archetypeQuery !== "") {
+        queryParameters.push(`archetype=${archetypeQuery}`);
+      }
+
+      let searchString = (
+        "https://db.ygoprodeck.com/api/v7/cardinfo.php?" +
+        queryParameters.join("&")
+      ).replace(" ", "%20");
+      console.log(searchString);
+
+      if (searchString !== "https://db.ygoprodeck.com/api/v7/cardinfo.php?") {
+        console.log("Searching...");
+        fetch(searchString)
+          .then((response) => response.json())
+          .then(({ data: cards }) => {
+            setCards(cards);
+          });
+      }
     }, 500);
     return () => clearTimeout(timeOut);
-  }, [searchQuery, typeQuery]);
+  }, [searchQuery, typeQuery, archetypeQuery]);
 
   return (
     <div className="Search container mx-auto w-4/5">
-      <div className="flex flex-col">
-        <div id="filter-search">
-          <label>Search</label>
-          <input type="search" onChange={e => setSearchQuery(e.target.value)} className="text-black" />
+      <div className="flex gap-4">
+        <div id="filter-search" className="flex flex-col gap-2">
+          <label className="font-medium">Search</label>
+          <input
+            type="search"
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="text-black"
+          />
         </div>
-        <div id="filter-type">
-          <label>Type</label>
-          <input type="search" onChange={e => setTypeQuery(e.target.value)} className="text-black" />
+        <div id="filter-type" className="flex flex-col gap-2">
+          <label className="font-medium">Type</label>
+          <select
+            onChange={(e) => setTypeQuery(e.target.value)}
+            className="text-black"
+          >
+            <option value="" selected>
+              Card type
+            </option>
+            {cardData.cardTypes.map((type) => (
+              <option key={type} value={type}>
+                {type}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div id="filter-archetype" className="flex flex-col gap-2">
+          <label className="font-medium">Archetype</label>
+          <select
+            onChange={(e) => setArchetypeQuery(e.target.value)}
+            className="text-black"
+          >
+            <option value="" selected>
+              Archetype
+            </option>
+            {archetypes.map((archetype) => (
+              <option key={archetype} value={archetype}>
+                {archetype}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
       <div className="flex flex-wrap justify-between gap-5">
-          {cards && cards.map(card => (
-              <Card name={card.name} type={card.type} url={card.card_images[0].image_url} atk={card.atk} def={card.def} owned="0" ran="0" key={card.name} />
+        {cards &&
+          cards.map((card) => (
+            <Card
+              name={card.name}
+              type={card.type}
+              url={card.card_images[0].image_url}
+              atk={card.atk}
+              def={card.def}
+              owned="0"
+              ran="0"
+              key={card.name}
+            />
           ))}
       </div>
     </div>
