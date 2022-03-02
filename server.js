b5b7ca7cfc9128ba8769dc07b8a729f1e09a0cac
Lily Howan <lily.howan@gmail.com>
2022-03-02 23:52:29 +1300

    Remove whitespace

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const routes = require('./routes/api');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes/api");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,11 +13,16 @@ mongoose
   .then(() => console.log("Database connected successfully"))
   .catch((err) => console.log(err));
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/api', routes);
+app.use("/api", routes);
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
