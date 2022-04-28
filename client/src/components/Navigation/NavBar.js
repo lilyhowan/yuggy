e6c215b683b81d7c2b65978fa2faea595757ed48
Lily Howan <lily.howan@gmail.com>
2022-04-28 23:01:23 +1200

    Simplify navbar

--- client/src/components/Navigation/NavBar.js
+++ client/src/components/Navigation/NavBar.js
@@ -3,18 +3,9 @@ import { NavLink, Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="navbar mt-1 mx-3 mb-10 text-neutral-content">
-      <div className="px-2 mx-2 navbar-start font-black text-primary">
-        <Link to="/">Yuggy</Link>
-      </div>
-      {/*<div className="hidden px-2 mx-2 navbar-center lg:flex">
-        <ul className="flex">
-          <li>Sign Up</li>
-        </ul>
-  </div>*/}
-      <div className="px-2 mx-2 navbar-end">
-        <NavLink to="/search">Search</NavLink>
-      </div>
+    <nav className="max-w-[100rem] mx-auto flex justify-between py-6">
+      <Link to="/" className="text-accent font-black">YUGGY</Link>
+      <NavLink to="/search">Search</NavLink>
     </nav>
   );
 }
